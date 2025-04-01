782f60f843315a40c2f6f7a4a33e9edb7f473a6b
minh13123 <minhluong1050@gmail.com>
2025-04-01 10:40:28 +0700

    Add file

--- PhoneStoreManagement/src/config/JDBCUtil.java
+++ PhoneStoreManagement/src/config/JDBCUtil.java
@@ -2,10 +2,6 @@ package config;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-<<<<<<< HEAD
-=======
-import java.sql.SQLException;
->>>>>>> huuloc
 import javax.swing.JOptionPane;
 
 public class JDBCUtil {
@@ -13,7 +9,6 @@ public class JDBCUtil {
     public static Connection getConnection() {
         Connection result = null;
         try {
-<<<<<<< HEAD
             // Dang ky MySQL Driver voi DriverManager
             DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
             //Cac thong so
@@ -24,26 +19,11 @@ public class JDBCUtil {
             result = DriverManager.getConnection(url, userName, password);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Không thể kết nối đến cơ sở dữ liệu !", "Lỗi", JOptionPane.ERROR_MESSAGE);
-=======
-            // Đăng ký MySQL Driver với DriverManager
-            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-            
-            // Các thông số kết nối
-            String url = "jdbc:mysql://localhost:3306/quanlybanhang?useSSL=false&serverTimezone=UTC";
-            String userName = "root";
-            String password = "";
-            
-            // Tạo kết nối
-            result = DriverManager.getConnection(url, userName, password);
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Lỗi kết nối CSDL: " + e.getMessage(), "Lỗi", JOptionPane.ERROR_MESSAGE);
->>>>>>> huuloc
         }
         return result;
     }
 
     public static void closeConnection(Connection c) {
-<<<<<<< HEAD
         try {
             if (c != null) {
                 c.close();
@@ -51,14 +31,6 @@ public class JDBCUtil {
         } catch (Exception e) {
             // TODO: handle exception
             e.printStackTrace();
-=======
-        if (c != null) {
-            try {
-                c.close();
-            } catch (SQLException e) {
-                JOptionPane.showMessageDialog(null, "Lỗi khi đóng kết nối: " + e.getMessage(), "Lỗi", JOptionPane.WARNING_MESSAGE);
-            }
->>>>>>> huuloc
         }
     }
 }
